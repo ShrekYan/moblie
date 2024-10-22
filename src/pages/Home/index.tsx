858d188a44230a5bba24324200de7cd8b2f86545
yanjinqiang <yanjinqiang@zofund.com>
2024-10-22 13:25:02 +0800

    feat:添加prettierrc功能

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,8 @@
-import React, {useState} from 'react'
-import reactLogo from './../../assets/react.svg'
-import viteLogo from './../../../public/vite.svg'
-import classnames from 'classnames'
-import styles from './index.module.scss'
+import React, { useState } from "react";
+import reactLogo from "./../../assets/react.svg";
+import viteLogo from "./../../../public/vite.svg";
+import classnames from "classnames";
+import styles from "./index.module.scss";
 
 const Home: React.FC = () => {
     const [count, setCount] = useState<number>(0);
@@ -11,10 +11,18 @@ const Home: React.FC = () => {
         <>
             <div>
                 <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className={styles.logo} alt="Vite logo"/>
+                    <img
+                        src={viteLogo}
+                        className={styles.logo}
+                        alt="Vite logo"
+                    />
                 </a>
                 <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className={classnames(styles.logo,styles.react)} alt="React logo"/>
+                    <img
+                        src={reactLogo}
+                        className={classnames(styles.logo, styles.react)}
+                        alt="React logo"
+                    />
                 </a>
             </div>
             <h1>Vite + React</h1>
@@ -24,14 +32,13 @@ const Home: React.FC = () => {
                 </button>
                 <p className={styles.pTest}>
                     Edit <code>src/App.tsx</code> and save to test HMR
-
                 </p>
             </div>
             <p className={styles.readTheDocs}>
                 Click on the Vite and React logos to learn more
             </p>
         </>
-    )
+    );
 };
 
 export default Home;
