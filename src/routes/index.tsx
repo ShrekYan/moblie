4d1e50905c221464dd39f43ab957083083325f7f
yanjinqiang <yanjinqiang@zofund.com>
2024-10-22 10:35:50 +0800

    test：测试prettierrc

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
-import {createHashRouter} from "react-router-dom"
-import Home from "./../pages/Home"
+import {createHashRouter} from 'react-router-dom'
+import Home from './../pages/Home'
 
 const router = createHashRouter([
     {
-        path: "/",
+        path: '/',
+
         element: <Home />
     }
 ])
