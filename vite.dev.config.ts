60ec6a0c3221ab94c59548f655ce7ab047dd6f3c
yanjinqiang <yanjinqiang@zofund.com>
2025-07-08 10:16:45 +0800

    fix: 优化别名配置

--- vite.dev.config.ts
+++ vite.dev.config.ts
@@ -56,7 +56,7 @@ export default (config: ConfigEnv) => {
                 "@business": path.resolve(__dirname, "./src/business"),
                 "@components": path.resolve(__dirname, "./src/components"),
                 "@core-tools": path.resolve(__dirname, "./src/core-tools"),
-                "@page/*": path.resolve(__dirname, "./src/pages"),
+                "@page": path.resolve(__dirname, "./src/pages"),
                 "@routes": path.resolve(__dirname, "./src/routes"),
                 "@local-types": path.resolve(__dirname, "./src/types"),
                 "@utils": path.resolve(__dirname, "./src/utils")
